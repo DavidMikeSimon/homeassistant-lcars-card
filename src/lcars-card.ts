5892079eaa209ee691c68ecd35caae55f0265249
David Simon <david.simon@color.com>
2021-09-18 15:22:56 -0700

    Removed a bunch of stuff

--- src/lcars-card.ts
+++ src/lcars-card.ts
@@ -7,31 +7,11 @@ import {
   PropertyValues,
   CSSResultGroup,
 } from 'lit';
-import { customElement, property, state } from "lit/decorators";
+import { customElement, property } from "lit/decorators";
 import {
   HomeAssistant,
-  hasConfigOrEntityChanged,
-  hasAction,
-  ActionHandlerEvent,
-  handleAction,
-  LovelaceCardEditor,
-  getLovelace,
 } from 'custom-card-helpers'; // This is a community maintained npm module with common helper functions/types. https://github.com/custom-cards/custom-card-helpers
 
-
-import './editor';
-
-import type { LcarsCardConfig } from './types';
-import { actionHandler } from './action-handler-directive';
-import { CARD_VERSION } from './const';
-import { localize } from './localize/localize';
-
-/* eslint no-console: 0 */
-console.info(
-  `%c  LCARS-CARD \n%c  ${localize('common.version')} ${CARD_VERSION}    `,
-  'color: orange; font-weight: bold; background: black',
-  'color: white; font-weight: bold; background: dimgray',
-);
 
 // This puts your card into the UI card picker dialog
 (window as any).customCards = (window as any).customCards || [];
@@ -43,10 +23,6 @@ console.info(
 
 @customElement('lcars-card')
 export class LcarsCard extends LitElement {
-  public static async getConfigElement(): Promise<LovelaceCardEditor> {
-    return document.createElement('lcars-card-editor');
-  }
-
   public static getStubConfig(): object {
     return {};
   }
@@ -55,81 +31,20 @@ export class LcarsCard extends LitElement {
   // https://lit.dev/docs/components/properties/
   @property({ attribute: false }) public hass!: HomeAssistant;
 
-  @state() private config!: LcarsCardConfig;
+  public setConfig(): void {}
 
-  // https://lit.dev/docs/components/properties/#accessors-custom
-  public setConfig(config: LcarsCardConfig): void {
-    // TODO Check for required fields and that they are of the proper format
-    if (!config) {
-      throw new Error(localize('common.invalid_configuration'));
-    }
-
-    if (config.test_gui) {
-      getLovelace().setEditMode(true);
-    }
-
-    this.config = {
-      name: 'LCARS',
-      ...config,
-    };
-  }
-
-  // https://lit.dev/docs/components/lifecycle/#reactive-update-cycle-performing
   protected shouldUpdate(changedProps: PropertyValues): boolean {
-    if (!this.config) {
-      return false;
-    }
-
-    return hasConfigOrEntityChanged(this, changedProps, false);
+    return !!changedProps;
   }
 
   // https://lit.dev/docs/components/rendering/
   protected render(): TemplateResult | void {
-    // TODO Check for stateObj or other necessary things and render a warning if missing
-    if (this.config.show_warning) {
-      return this._showWarning(localize('common.show_warning'));
-    }
-
-    if (this.config.show_error) {
-      return this._showError(localize('common.show_error'));
-    }
-
     return html`
       <ha-card
-        .header=${this.config.name}
-        @action=${this._handleAction}
-        .actionHandler=${actionHandler({
-          hasHold: hasAction(this.config.hold_action),
-          hasDoubleClick: hasAction(this.config.double_tap_action),
-        })}
+        header="This is a thing?"
         tabindex="0"
         .label="LCARS"
       ></ha-card>
-    `;
-  }
-
-  private _handleAction(ev: ActionHandlerEvent): void {
-    if (this.hass && this.config && ev.detail.action) {
-      handleAction(this, this.hass, this.config, ev.detail.action);
-    }
-  }
-
-  private _showWarning(warning: string): TemplateResult {
-    return html`
-      <hui-warning>${warning}</hui-warning>
-    `;
-  }
-
-  private _showError(error: string): TemplateResult {
-    const errorCard = document.createElement('hui-error-card');
-    errorCard.setConfig({
-      type: 'error',
-      error,
-      origConfig: this.config,
-    });
-
-    return html`
-      ${errorCard}
     `;
   }
 
