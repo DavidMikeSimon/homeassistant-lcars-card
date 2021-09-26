4f6880c15f873ed935ec9ef8fe5e1ae0913a62a3
David Simon <david.simon@color.com>
2021-09-25 21:07:13 -0700

    Got the 'App' button working

--- src/packages/hass-lcars-react-components/src/VideoControls.tsx
+++ src/packages/hass-lcars-react-components/src/VideoControls.tsx
@@ -3,13 +3,23 @@ import { JSX } from 'preact';
 import { useHassContext } from './hassContext';
 
 export function VideoControls(props): JSX.Element {
+  const { mediaEntityId } = props;
   const { hass } = useHassContext();
-  const mediaEntity = hass.states[props.mediaEntityId];
+  const mediaEntity = hass.states[mediaEntityId];
+  const remoteEntity = hass.states[mediaEntityId.replace("media_player", "remote")];
 
   function mediaCommand(command: string, extraArgs = {}): void {
     hass.callService('media_player', command, {
       entity_id: mediaEntity.entity_id,
       ...extraArgs
+    });
+  }
+
+  function remoteCommand(command: string): void {
+    if (!remoteEntity) { return };
+    hass.callService('remote', 'send_command', {
+      entity_id: remoteEntity.entity_id,
+      command
     });
   }
 
@@ -33,7 +43,7 @@ export function VideoControls(props): JSX.Element {
           <div class="lcars-element button" onClick={() => setSource('HDMI 3/ARC') }>PS4</div>
           <div class="lcars-element button" onClick={() => setSource('HDMI 2') }>SWT</div>
           <div class="lcars-element button" onClick={() => setSource('HDMI 1') }>DNNS</div>
-          <div class="lcars-element button" onClick={() => setSource('Home') }>APP</div>
+          <div class="lcars-element button" onClick={() => remoteCommand('Home') }>APP</div>
         </div>
         <div class="lcars-row">
           <div class="lcars-element lcars-u-1-2 button" onClick={() => mediaCommand('turn_off')}>OFF</div>
